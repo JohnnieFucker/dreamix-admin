54e4b4c84a4524cf1f9e4360d98fed16f2c5ce98
ShineLee <shine@vip.qq.com>
2017-11-15 20:39:23 +0800

    fix bug

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ module.exports.createMasterConsole = consoleService.createMasterConsole;
 module.exports.createMonitorConsole = consoleService.createMonitorConsole;
 module.exports.adminClient = require('./lib/client/client');
 
-exports.modules = {};
+module.exports.modules = {};
 
 fs.readdirSync(`${__dirname}/lib/modules`).forEach((filename) => {
     if (/\.js$/.test(filename)) {
         const name = filename.substr(0, filename.lastIndexOf('.'));
         const _module = require(`./lib/modules/${name}`);// eslint-disable-line
         if (!_module.moduleError) {
-            Object.defineProperty(exports.modules, name, {
-                get: _module,
+            Object.defineProperty(module.exports.modules, name, {
+                get: () => _module,
                 enumerable: true,
                 configurable: true
             });
